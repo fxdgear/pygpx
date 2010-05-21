ef55d4f5c0f5b361d6f17df60a891309ffeb0e11
Nick Lang <nicholas@nick-langs-macbook-pro-44.local>
2010-05-20 19:37:42 -0500

    adding MIT license and updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-version = "0.2"
+version = "0.3"
 
 """Setup script for pygpx"""
 
@@ -11,10 +11,10 @@ setup (
     version = version,
     description = "A module for parsing GPX files.",
     long_description = "This module allows you to parse and extract data from GPX files.",
-    author = "Ben Leslie",
-    author_email = "benno@benno.id.au",
-    url = "http://www.benno.id.au/code/pygpx/",
-    download_url = "http://www.benno.id.au/code/pygpx/pygpx-%s.tar.gz" % version,
+    author = "Ben Leslie, Nick Lang",
+    author_email = "benno@benno.id.au, nick.lang@gmail.com",
+    url = "http://www.github.com/fxdgear/pygpx",
+    download_url = "http://github.com/fxdgear/pygpx/zipball/master",
     license = "http://www.opensource.org/licenses/mit-license.php",
     py_modules = ["pygpx"],
     platforms = ["any"],
