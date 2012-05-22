8d17b6b1204643d45408ab47cbd66abca59ff6ca
Nick Lang <nick@nicklang.com>
2012-05-22 16:44:41 -0600

    adding some more useful features to pygpx

--- src/pygpx/pygpx.py
+++ src/pygpx/pygpx.py
@@ -3,28 +3,16 @@ A module for parsing GPX files.
 """
 from lxml import etree
 import os
+from decimal import Decimal
 import math
 import datetime
+from dateutil.parser import parse
+
 
 def deg2rad(deg):
     """Convert degrees to radians"""
-    return deg / (180 / math.pi)
+    return deg / (180 / Decimal(math.pi))
 
-def datetime_iso(string):
-    """Parse an ISO formatted string. E.g:
-    2006-01-08T06:45:07Z
-
-    Return a datetime object.
-    """
-    yr = int(string[0:4])
-    mn = int(string[5:7])
-    da = int(string[8:10])
-    hr = int(string[11:13])
-    mi = int(string[14:16])
-    se = int(string[17:19])
-    #tz = string[19:]
-    dt = datetime.datetime(yr, mn, da, hr, mi, se, )
-    return dt
 
 class GPXTrackPt:
     """A track point."""
@@ -32,15 +20,15 @@ class GPXTrackPt:
     def __init__(self, node, version):
         """Construct a trackpint given an XML node."""
         self.version = version
-        self.lat = float(node.get("lat"))
-        self.lon = float(node.get("lon"))
+        self.lat = Decimal(node.get("lat"))
+        self.lon = Decimal(node.get("lon"))
         self.elevation = None
         self.time = None
         for child in node:
             if child.tag == "{http://www.topografix.com/GPX/1/1}time":
-                self.time = datetime_iso(child.text)
+                self.time = parse(child.text)
             elif child.tag == "{http://www.topografix.com/GPX/1/1}ele":
-                self.elevation = float(child.text)
+                self.elevation = Decimal(child.text)
             elif child.tag == "{http://www.topografix.com/GPX/1/1}extensions":
                 pass
             else:
@@ -48,33 +36,61 @@ class GPXTrackPt:
 
     def distance(self, other):
         """Compute the distance from this point to another."""
-        r = 6378700
-        dist = r * math.acos(math.sin(deg2rad(self.lat)) *
-                             math.sin(deg2rad(other.lat)) + 
-                             math.cos(deg2rad(self.lat)) *
-                             math.cos(deg2rad(other.lat)) * 
-                             math.cos(deg2rad(self.lon - other.lon)))
-        return dist
+        r = Decimal(6378700)
+        try:
+            dist = r * Decimal(
+                math.acos(
+                    round(
+                        Decimal(math.sin(deg2rad(self.lat))) *
+                        Decimal(math.sin(deg2rad(other.lat))) +
+                        Decimal(math.cos(deg2rad(self.lat))) *
+                        Decimal(math.cos(deg2rad(other.lat))) *
+                        Decimal(math.cos(deg2rad(self.lon - other.lon))),
+                        12)
+                )
+            )
+        except ValueError, e:
+            raise ValueError(e)
+
+            print self.lat, self.lon
+        return Decimal(dist)
 
     def duration(self, other):
         """Return the time difference between two nodes."""
         return other.time - self.time
 
+
 class GPXTrackSeg:
     """One segment of a GPX track."""
-    
+
     def __init__(self, node, version):
         self.version = version
         self.trkpts = []
+        self.elevation_gain = Decimal(0.0)
+        self.elevation_loss = Decimal(0.0)
         for child in node:
             if child.tag == "{http://www.topografix.com/GPX/1/1}trkpt":
                 self.trkpts.append(GPXTrackPt(child, self.version))
             else:
-                raise ValueError, "Can't handle node <%s>" % node.nodeName
+                raise ValueError("Can't handle node <%s>" % node.nodeName)
+        self._get_elevation()
+
+    def _get_elevation(self):
+        gain = Decimal(0.0)
+        loss = Decimal(0.0)
+        last_pt = self.trkpts[0]
+        last_elevation = last_pt.elevation
+        for pt in self.trkpts[1:]:
+            if pt.elevation > last_elevation:
+                gain += pt.elevation
+            else:
+                loss += pt.elevation
+        self.elevation_gain = gain
+        self.elevation_loss = loss
 
     def distance(self):
         """Return the distance along the track segment."""
-        _length = 0.0
+        _length = Decimal(0.0)
         last_pt = self.trkpts[0]
         for pt in self.trkpts[1:]:
             _length += last_pt.distance(pt)
@@ -85,24 +101,31 @@ class GPXTrackSeg:
         """Return the duration of this track segment."""
         return self.trkpts[0].duration(self.trkpts[-1])
 
+
 class GPXTrack:
     """A GPX Track, made up of individual segments."""
-    
+
     def __init__(self, node, version):
         """Create a GPX track, give an XML DOM node and the document version."""
-        
+
         self.version = version
         self.trksegs = []
         for child in node:
             if child.tag == "{http://www.topografix.com/GPX/1/1}name":
-                self.name = child.text              
+                self.name = child.text
             elif child.tag == "{http://www.topografix.com/GPX/1/1}trkseg":
                 if len(child) > 0:
                     self.trksegs.append(GPXTrackSeg(child, self.version))
             elif child.tag == "{http://www.topografix.com/GPX/1/1}number":
-                self.name = child.text               
+                self.name = child.text
             else:
-                raise ValueError, "Can't handle node <%s>" % node.nodeName
+                raise ValueError("Can't handle node <%s>" % node.nodeName)
+
+    def elevation_gain(self):
+        return sum([trkseg.elevation_gain for trkseg in self.trksegs])
+
+    def elevation_loss(self):
+        return sum([trkseg.elevation_loss for trkseg in self.trksegs])
 
     def distance(self):
         """Return the distance for this track."""
@@ -110,7 +133,7 @@ class GPXTrack:
             return sum([trkseg.distance() for trkseg in self.trksegs])
         except IndexError:
             print "emtpy track segment"
-        
+
     def duration(self):
         """Return the duration for this track. The sum of the duration
         of all track segments."""
@@ -118,7 +141,7 @@ class GPXTrack:
         for trkseg in self.trksegs:
             dur += trkseg.duration()
         return dur
-    
+
     def full_duration(self):
         """Return the full duration of the track. This does not include
         break time."""
@@ -126,11 +149,17 @@ class GPXTrack:
 
     def start(self):
         """Return the starting track point."""
-        return self.trksegs[0].trkpts[0]
+        try:
+            return self.trksegs[0].trkpts[0]
+        except IndexError:
+            return None
 
     def end(self):
         """Return the final track point of this track."""
-        return self.trksegs[-1].trkpts[-1]
+        try:
+            return self.trksegs[-1].trkpts[-1]
+        except IndexError:
+            return None
 
     def start_time(self):
         """Return the start time of the track."""
@@ -143,13 +172,13 @@ class GPXTrack:
 
 class GPX:
     """This class allows the manipulation of a GPX document."""
-    
+
     def __init__(self, fd):
         """
         Create a GPX object based on the given file descriptor.
         """
         PATH = os.path.dirname(__file__)
-        SCHEMA = os.path.join(PATH, "schema","gpx-1_1.xsd")
+        SCHEMA = os.path.join(PATH, "schema", "gpx-1_1.xsd")
         self.creator = None
         self.time = None
         self.tracks = []
@@ -159,23 +188,23 @@ class GPX:
         gpx_schema = etree.XMLSchema(gpx_schema_doc)
         self.gpx_doc = etree.parse(fd)
         self.root = self.gpx_doc.getroot()
-        
+
         # Test if this is a GPX file or not and if it's version 1.1
         if self.root.tag != "{http://www.topografix.com/GPX/1/1}gpx":
             raise ValueError("Not a gpx file: '%s'" % fd)
         elif self.root.get("version") != "1.1":
-            raise ValueError("Verion %s is not supported. Must use GPX v1.1" % root.get("version"))
+            raise ValueError("Verion %s is not supported. Must use GPX v1.1" % self.root.get("version"))
         else:
             self.version = self.root.get("version")
         # attempt to validate the xml file against the schema
         try:
             gpx_schema.validate(self.gpx_doc)
         except:
-            raise Exception("GPX does not validate: '%s'" % fd) 
-        
+            raise Exception("GPX does not validate: '%s'" % fd)
+
         # initalize the GPX document for parsing.
         self._init_version()
-        
+
     def _init_version(self):
         """
         Initialize a GPX instance
@@ -188,4 +217,52 @@ class GPX:
                 self.tracks.append(GPXTrack(child, self.version))
             elif child.tag == "{http://www.topografix.com/GPX/1/1}metadata":
                 self.metadata = child
-        
+
+    def elevation_gain(self):
+        return sum([track.elevation_gain() for track in self.tracks])
+
+    def elevation_loss(self):
+        return sum([track.elevation_loss() for track in self.tracks])
+
+    def distance(self):
+        """Return the distance for this gpx file."""
+        try:
+            return sum([track.distance() for track in self.tracks])
+        except IndexError:
+            print "emtpy GPX file"
+
+    def duration(self):
+        """Return the duration for this track. The sum of the duration
+        of all track segments."""
+        dur = datetime.timedelta(0)
+        for track in self.tracks:
+            dur += track.duration()
+        return dur
+
+    def full_duration(self):
+        """Return the full duration of the track. This does not include
+        break time."""
+        if hasattr(self.start(), 'duration'):
+            return self.start().duration(self.end())
+        else:
+            return None
+
+    def start(self):
+        """Return the starting track point."""
+        return self.tracks[0].start()
+
+    def end(self):
+        """Return the final track point of this track."""
+        return self.tracks[-1].end()
+
+    def start_time(self):
+        """Return the start time of the track."""
+        if self.start():
+            return self.start().time
+        else:
+            return None
+
+    def end_time(self):
+        """Return the end time of te track."""
+        return self.end().time
+
